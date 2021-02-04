5f90df305a169681ccb0a8724af324263cd22167
daidk <65601527+daidk@users.noreply.github.com>
2021-02-03 21:19:12 -0500

    Add files via upload

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,22 +1,26 @@
+#!/usr/bin/env bash
+
 function guess_file_number_func(){
-    correct_file_number=$(ls -l | wc -l)
-    while true;
-    do
-        echo "Please guess how many files are in the current directory"
-        read  guess_file_number
-        if [ $guess_file_number -eq $correct_file_number ]
-        then
-            echo "Congratulations! You have correctly guessed the number of files"
-        elif [ $number -gt $true_ans ]
-        then
-            echo "Your guess is greater than the correct number of files"
-        else
-            echo "Your guess is smaller than the correct number of files"
-        break;
-        fi
-    done
+	correct_file_number=$(ls -l | wc -l)
+	while true;
+	do
+		echo "Please guess how many files are in the current directory"
+		read  guess_file_number
+		if [ $guess_file_number -eq $correct_file_number ]
+		then
+			echo "Congratulations! You have correctly guessed the number of files"
+		elif [ $guess_file_number -gt $correct_file_number ]
+		then
+			echo "Your guess is greater than the correct number of files"
+		else
+			echo "Your guess is smaller than the correct number of files"
+		fi
+	break;
+	done
 }
-echo "guess the files number in the current directory!"
+echo "guess the number of files in the current directory!"
     
-guess_file_number
+guess_file_number_func
+
+
     
